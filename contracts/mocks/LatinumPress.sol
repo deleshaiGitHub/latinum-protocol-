0835b04634062f269fd64e14820904937982cc5c
Mark <88356506+deleshaiGitHub@users.noreply.github.com>
2026-02-27 09:08:11 -0600

    Update LatinumPress.sol
    live on base testnet
    0x4e1Ee74f7cB0Ee201f267139fA683ad4A2294C3c

--- contracts/mocks/LatinumPress.sol
+++ contracts/mocks/LatinumPress.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IGPL {
@@ -10,48 +9,64 @@ interface IGPL {
 }
 
 contract LatinumPress is Ownable {
-    using SafeERC20 for IERC20;
-
-    uint256 public constant MINT_RATE = 500_000;           
-    uint256 public constant PLAN_B_TIMER = 180 days;       
-    uint256 public constant HARD_CAP = 1_000_000 * 10**6;  
-    uint256 public constant NAGUS_FEE_BPS = 25;            
-    uint256 public constant BPS_DENOMINATOR = 10000;
-
+    // Constants from Whitepaper v1.1
+    uint256 public constant MINT_RATE = 500_000; // 1 USDC : 500k GPL
+    uint256 public constant NAGUS_FEE_BPS = 25;  // 0.25%
+    uint256 public constant HARD_CAP_USDC = 1_000_000 * 10**6;
+    
     IERC20 public immutable usdc;
     IGPL public immutable gpl;
-    address public nagusVault;
+    
+    uint256 public totalUsdcCollected;
+    address public nagusReserve;
+    bool public isGlobalThresholdMet = false;
 
-    uint256 public totalRaised;
-    uint256 public deploymentTime;
-    bool public pressClosed;
+    mapping(address => uint256) public lockedBalances;
 
-    constructor(address _usdc, address _gpl, address _nagusVault) Ownable(msg.sender) {
+    event Minted(address indexed user, uint256 usdcIn, uint256 gplOut);
+    event ThresholdReached();
+
+    constructor(address _usdc, address _gpl, address _nagus) Ownable(msg.sender) {
         usdc = IERC20(_usdc);
         gpl = IGPL(_gpl);
-        nagusVault = _nagusVault;
-        deploymentTime = block.timestamp;
+        nagusReserve = _nagus;
     }
 
-    function pressLatinum(uint256 _amountUSDC) external {
-        require(!pressClosed, "Press Closed");
-        require(block.timestamp <= deploymentTime + PLAN_B_TIMER, "Plan B Active");
-        require(totalRaised + _amountUSDC <= HARD_CAP, "Hard Cap Reached");
+    /**
+     * @dev Primary Minting Function
+     * @param _usdcAmount Amount of USDC to spend (6 decimals)
+     */
+    function mintGPL(uint256 _usdcAmount) external {
+        require(!isGlobalThresholdMet, "Latinum: Hard Cap reached");
+        require(totalUsdcCollected + _usdcAmount <= HARD_CAP_USDC, "Latinum: Exceeds Hard Cap");
 
-        uint256 nagusAmount = (_amountUSDC * NAGUS_FEE_BPS) / BPS_DENOMINATOR;
-        uint256 treasuryAmount = _amountUSDC - nagusAmount;
+        // 1. Transfer USDC from user
+        usdc.transferFrom(msg.sender, address(this), _usdcAmount);
 
-        usdc.safeTransferFrom(msg.sender, nagusVault, nagusAmount);
-        usdc.safeTransferFrom(msg.sender, address(this), treasuryAmount);
+        // 2. Calculate Nagus Fee (0.25%)
+        uint256 fee = (_usdcAmount * NAGUS_FEE_BPS) / 10000;
+        uint256 netUsdc = _usdcAmount - fee;
+        usdc.transfer(nagusReserve, fee);
 
-        uint256 gplToMint = (_amountUSDC * 10**12) * MINT_RATE;
-        totalRaised += _amountUSDC;
+        // 3. Calculate GPL Output (Math: USDC_Amount * 10^12 * 500,000)
+        // Adjusting 6 decimals to 18 decimals: 10^12
+        uint256 gplAmount = netUsdc * 10**12 * MINT_RATE;
 
-        gpl.mint(msg.sender, gplToMint);
+        // 4. Update state and Mint to this contract (GestationVault logic)
+        totalUsdcCollected += _usdcAmount;
+        lockedBalances[msg.sender] += gplAmount;
+        
+        gpl.mint(address(this), gplAmount);
+
+        if (totalUsdcCollected >= HARD_CAP_USDC) {
+            isGlobalThresholdMet = true;
+            emit ThresholdReached();
+        }
+
+        emit Minted(msg.sender, _usdcAmount, gplAmount);
     }
 
-    function triggerPlanB() external {
-        require(block.timestamp > deploymentTime + PLAN_B_TIMER, "Too early");
-        pressClosed = true;
+    function setNagusReserve(address _newNagus) external onlyOwner {
+        nagusReserve = _newNagus;
     }
 }

3598256eb9dfaeaea30e6594f7a4515188bf4345
Mark <88356506+deleshaiGitHub@users.noreply.github.com>
2026-02-27 09:06:51 -0600

    Update GPL.sol
    live on base testnet
    GPL 0xD816C8083A7150D2D9f545e6ECd9d612115C6B4a

--- contracts/mocks/GPL.sol
+++ contracts/mocks/GPL.sol
@@ -1,26 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {OFT} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract GoldPressedLatinum is ERC20, Ownable {
+/**
+ * @title GoldPressedLatinum ($GPL)
+ * @dev Omnichain Fungible Token (OFT) V2 for the Latinum Protocol.
+ * Base Sepolia Endpoint: 0x6EDCE65403992e310A62460808c4b910D972f10f
+ */
+contract GoldPressedLatinum is OFT {
+    // 500 Billion Max Supply (18 decimals)
     uint256 public constant MAX_SUPPLY = 500_000_000_000 * 10**18;
-    address public latinumPress;
 
-    constructor() ERC20("Gold Pressed Latinum", "GPL") Ownable(msg.sender) {}
+    constructor(
+        address _lzEndpoint,
+        address _delegate
+    ) 
+        OFT("Gold-Pressed Latinum", "GPL", _lzEndpoint, _delegate) 
+        Ownable(_delegate) 
+    {}
 
-    modifier onlyPress() {
-        require(msg.sender == latinumPress || msg.sender == owner(), "GPL: Unauthorized");
-        _;
+    /**
+     * @dev Enforces the 500B Hard Cap from the Whitepaper.
+     * Only the LatinumPress (once deployed and set as owner) will call this.
+     */
+    function mint(address _to, uint256 _amount) external onlyOwner {
+        require(totalSupply() + _amount <= MAX_SUPPLY, "Latinum: Hard Cap Exceeded");
+        _mint(_to, _amount);
     }
 
-    function setPress(address _press) external onlyOwner { 
-        latinumPress = _press; 
-    }
-
-    function mint(address to, uint256 amount) external onlyPress {
-        require(totalSupply() + amount <= MAX_SUPPLY, "GPL: Cap Exceeded");
-        _mint(to, amount);
+    function burn(uint256 _amount) external {
+        _burn(msg.sender, _amount);
     }
 }
